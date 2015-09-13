a09ec75cc05645ac9e7c2eafe11834a41dab446a
Danyil Bohdan <danyil.bohdan@gmail.com>
2015-09-13 16:55:25 +0300

    example3/run.sh: Start chat server w/ websocketd

--- example3/run.sh
+++ example3/run.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+set -e
+if [ "$1" = "py" ] || [ "$1" = "tcl" ]; then
+    echo "Starting chat.$1"
+    ./chat.$1 &
+else
+    echo "usage: $0 (py|tcl)"
+    exit 1
+fi
+trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT # Kill the chat server on exit.
 ../websocketd --port=8080 --staticdir=. nc localhost 7777
